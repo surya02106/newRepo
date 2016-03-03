1b404c7b390956fff98df742ac3c2401f0af2ec4
surya02106 <santhu02106@gmail.com>
2016-03-03 13:03:52 +0530

    Commit to Delete

--- src/main/java/org/com/services/LoginAuthentication.java
+++
@@ -1,7 +0,0 @@
-package org.com.services;
-
-import org.com.model.User;
-
-public interface LoginAuthentication {
-	public User authenticateuser(User user);
-}
--- src/main/java/org/com/services/UserRegistration.java
+++
@@ -1,9 +0,0 @@
-package org.com.services;
-
-import org.com.model.User;
-
-public interface UserRegistration {
-	User userRegister(User user);
-	String userValidation(User user);
-	String checkForUser(User user);
-}
